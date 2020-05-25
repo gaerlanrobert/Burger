8c3a318fc3d0261e7b898fed2fb464173ea68a73
Your NameRobert <57042633+gaerlanrobert@users.noreply.github.com>
2020-05-25 13:34:01 -0700

    added files

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id int NOT NULL,
+    AUTO_INCREMENT,
+    burger_name VARCHAR
+    (50),
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY
+    (id)
+);
